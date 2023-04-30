5cc94845e851e35f2f4c9d631503a8ee6e715416
Francis Ngo <francis.t.ngo@gmail.com>
2023-04-29 23:36:07 -0700

    Version api routes

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,11 +1,10 @@
 const express = require("express");
+const launchesRouter = express.Router();
 const {
 	httpGetAllLaunches,
 	httpAddNewLaunch,
 	httpAbortLaunch,
 } = require("./launches.controller");
-
-const launchesRouter = express.Router();
 
 launchesRouter.get("/", httpGetAllLaunches);
 launchesRouter.post("/", httpAddNewLaunch);
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { httpGetAllPlanets } = require("./planets.controller");
 const planetsRouter = express.Router();
+const { httpGetAllPlanets } = require("./planets.controller");
 
 planetsRouter.get("/", httpGetAllPlanets);
 
